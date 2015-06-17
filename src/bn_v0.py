fixed the softmax

src/bn_v0.py
@@ -7,15 +7,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cPickle as pkl
 import sys
+from input_gen import inputs_gen
 
 if __name__=="__main__":
 
     t_img,t_label=dl.training_load()
     s_img,s_label=dl.test_load()
 
-    with open('initial_conf.pickle','rb') as f_init:
-        data=pkl.load(f_init)
-
+    data=pkl.loads(inputs_gen())
+    
     training_index=data['training_index']
     testing_index=data['testing_index']
     train_input=np.array(t_img[training_index])
@@ -31,7 +31,7 @@ if __name__=="__main__":
     num_of_trains=len(train_input)
     num_of_tests=len(test_input)
     
-    learnrate=data['learnrate']
+    learnrate=data['lrate']
     lrate_decay=data['lrate_decay']
     batchsize=data['batchsize']
     epochs=data['epochs']
@@ -81,7 +81,7 @@ if __name__=="__main__":
         plt.ylabel('Loss')
         plt.xlabel('Epochs')
 
-        plt.savefig('../results/bnv0_TestSet.png')
+        # plt.savefig('../results/bnv0_TestSet.png') 
         plt.show()
 
     if train_check:
@@ -107,10 +107,10 @@ if __name__=="__main__":
         plt.grid()
         plt.ylabel('Loss')
         plt.xlabel('Epochs')
-
-        plt.savefig('../results/bnv0_TrainSet.png')
+ 
+        # plt.savefig('../results/bnv0_TrainSet.png')
         plt.show()
 
-    if test_check or train_check:
-        with open("../results/bnv0_accuracy.pickle",'w') as frec:
-            pkl.dump(data,frec)
+    # if test_check or train_check:
+    #     with open("../results/bnv0_accuracy.pickle",'w') as frec:
+    #         pkl.dump(data,frec)

src/input_gen.py
@@ -7,7 +7,7 @@ import numpy as np
 import cPickle as pkl
 import random as rnd
 
-def inputs_gen(args):
+def inputs_gen():
 
     input_data=[]
     
@@ -23,8 +23,8 @@ def inputs_gen(args):
     idx_test=np.array(rnd.sample(range(totaltest),numoftests))
 
     layers=[28*28,100,100,100,10]
-    ubd=1.
-    lbd=-1.
+    ubd=np.sqrt(3.)
+    lbd=-np.sqrt(3.)
     weights=[np.random.uniform(lbd,ubd,[x,y])
              for x,y in zip(layers[:-1],layers[1:])]
     bias=[np.random.uniform(lbd,ubd,x) for x in layers[1:]]
@@ -48,9 +48,6 @@ def inputs_gen(args):
     data['train_check']=False
     data['init']='constVar1'
     data['lrate_decay']=0.0
-    
-    for arg in args:
-        data['learnrate']=arg
-        input_data.append(pkl.dumps(data))
-
-    return input_data
+    data['lrate']=0.1
+ 
+    return pkl.dumps(data)

src/network_bn_v1.py
@@ -69,18 +69,18 @@ class BNv1:
                     rec_tmp=[np.mean(x,0) for x in self.deltas]
                     rec_d.append(rec_tmp)
                     rec_dw.append(dw)
-                # mini-batch update performance
-                if test_check and not (q+1)%rec_freq:
-                    bs_inf=60
-                    ep_inf=3
-                    self.pop_stats(train_inputs,ep_inf,bs_inf)
-                    accu,cost=self.inference(test_inputs,test_labels)
-                    self.test_accu.append(accu)
-                    self.test_cost.append(cost)
-                if train_check and not (q+1)%rec_freq:
-                    accu,cost=self.inference(train_inputs,train_labels)
-                    self.train_accu.append(accu)
-                    self.train_cost.append(cost)
+            # mini-batch update performance
+            if test_check and not (q+1)%rec_freq:
+                bs_inf=60
+                ep_inf=3
+                self.pop_stats(train_inputs,ep_inf,bs_inf)
+                accu,cost=self.inference(test_inputs,test_labels)
+                self.test_accu.append(accu)
+                self.test_cost.append(cost)
+            if train_check and not (q+1)%rec_freq:
+                accu,cost=self.inference(train_inputs,train_labels)
+                self.train_accu.append(accu)
+                self.train_cost.append(cost)
 
 
             tend=time.clock()